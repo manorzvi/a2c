5b8622e5ee76b96daeb10bb4f50ff79d93222576
manorzvi <manor.zvi@campus.technion.ac.il>
2021-06-05 10:49:24 +0300

    first commit

---
+++ train.py
@@ -0,0 +1,126 @@
+import os
+import json
+import torch
+from utils import np2torch_obs
+from torch.distributions import Categorical
+import numpy as np
+from tqdm import tqdm
+from loguru import logger
+
+from baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
+
+from utils import training_log_msg, playing_log_msg, save_checkpoint
+from play import trained_agent_play
+
+
+def train(model, env, value_loss_func, optimizer, args):
+    model.train()
+
+    if env.observation_space.__class__.__name__ == 'Box':
+        obs_shape = env.observation_space.shape
+        obs_shape = (obs_shape[2], obs_shape[0], obs_shape[1])
+    else:
+        raise ValueError(f"env.observation_space.__class__.__name__ == {env.observation_space.__class__.__name__}.\n"
+                         f"Only 'Box' currently supported.")
+
+    trace_obs = torch.zeros(args.n_steps + 1, args.n_env, *obs_shape).to(args.device)  # TODO: check the impact on performance of moving the tracers to 'registered buffers' of the model (saved on gpu memory)
+    trace_reward = torch.zeros(args.n_steps, args.n_env).to(args.device)
+    trace_return = torch.zeros(args.n_steps + 1, args.n_env).to(args.device)
+    trace_action = torch.zeros(args.n_steps, args.n_env).to(args.device)
+    trace_mask = torch.zeros(args.n_steps, args.n_env).to(args.device)
+
+    obs = env.reset()
+    # env.render()
+    obs = np2torch_obs(obs, env.observation_space.low, env.observation_space.high)
+    obs.to(args.device)
+
+    trace_obs[0] = obs
+
+    average_episode_reward = []
+    average_episode_length = []
+
+    episode_reward = [0 for _ in range(args.n_env)]
+    episode_length = [0 for _ in range(args.n_env)]
+
+    for update in tqdm(range(args.n_updates)):
+
+        for step in range(args.n_steps):
+
+            episode_length = [x+1 for x in episode_length]
+
+            with torch.no_grad():
+                policies = model.forward_actor(trace_obs[step])
+
+            m = Categorical(logits=policies)
+            actions = m.sample()  # TODO: maybe need here to move actions (before env.step) back to cpu (need to check on gpu)
+
+            obs, rewards, dones, infos = env.step(actions.tolist())
+            # env.render()
+            obs = np2torch_obs(obs, env.observation_space.low, env.observation_space.high)
+            obs.to(args.device)
+
+            episode_reward = [x+r for x, r in zip(episode_reward, rewards)]
+
+            trace_obs[step + 1] = obs
+            trace_action[step] = actions.to(args.device)
+            trace_reward[step] = torch.from_numpy(rewards).to(args.device)
+            trace_mask[step] = torch.from_numpy(1 - dones.astype(np.float32)).to(args.device)
+
+            done = dones if not isinstance(env, SubprocVecEnv) else any(dones)
+            if done:
+                average_episode_length.extend([l for l, d in zip(episode_length, dones) if d])
+                average_episode_reward.extend([r for r, d in zip(episode_reward, dones) if d])
+                episode_length = [l * (1 - d.astype(int)) for l, d in zip(episode_length, dones)]
+                episode_reward = [r * (1 - d.astype(int)) for r, d in zip(episode_reward, dones)]
+
+        del step
+
+        with torch.no_grad():
+            next_value = model.forward_critic(trace_obs[-1])
+
+        trace_return[-1] = next_value.squeeze(1)
+        for step in reversed(range(args.n_steps)):
+            trace_return[step] = trace_reward[step] + args.gamma * trace_return[step + 1] * trace_mask[step]
+
+        policies, values = model(trace_obs[:-1].view(-1, *obs_shape))
+        m = Categorical(logits=policies)
+        log_probs = m.log_prob(trace_action.view(-1))
+        entropy_probs = m.entropy().mean()  # TODO: Consider to subtract the action entropy from the action loss, as in: 'Diversity Actor-Critic: Sample-Aware Entropy Regularization for Sample-Efficient Exploration' (manorz, 06/03/21)
+
+        values = values.view(args.n_steps, args.n_env)
+        log_probs = log_probs.view(args.n_steps, args.n_env)
+        advantage = trace_return[:-1] - values
+
+        value_loss = value_loss_func(trace_return[:-1], values)
+        action_loss = -(advantage.detach() * log_probs).mean()
+
+        if args.log:
+            if (update * args.n_steps) % args.log_interval == 0:
+                log_msg = training_log_msg(update, action_loss.item(), value_loss.item(), entropy_probs.item(),
+                                           sum(average_episode_reward)/len(average_episode_reward) if len(average_episode_reward) > 0 else None,
+                                           sum(average_episode_length)/len(average_episode_length) if len(average_episode_length) > 0 else None, args)
+                logger.info(log_msg)
+
+        if args.save:
+            if (update * args.n_steps) % args.save_interval == 0:
+                checkpoint_name = '{}.pth'.format(update * args.n_steps)
+                checkpoint_name = os.path.expanduser(os.path.join(args.save_path, checkpoint_name))
+                logger.info('Saving checkpoint {} at {} ...'.format(checkpoint_name, update * args.n_steps))
+                save_checkpoint(model, optimizer, update, value_loss.item(), action_loss.item(),
+                                entropy_probs.item(), average_episode_reward, average_episode_length, checkpoint_name,
+                                args, first=(update == 0))
+                logger.info('Done.')
+
+        if args.evaluate:
+            if (update * args.n_steps) % args.evaluate_interval == 0:
+                logger.info(playing_log_msg(trained_agent_play(model, args)))
+
+        optimizer.zero_grad()
+        value_loss.backward(retain_graph=True)
+        action_loss.backward()
+        optimizer.step()
+
+        trace_obs[0]    = trace_obs[-1]
+        trace_mask[0]   = trace_mask[-1]
+
+    model.eval()
---
+++ utils.py
@@ -0,0 +1,108 @@
+import os
+import json
+from collections import defaultdict
+from typing import List
+import numpy as np
+import torch
+import gym
+
+
+def defaultdict_gym_envs() -> defaultdict:
+    envs = defaultdict(set)
+    for env in gym.envs.registry.all():
+        env_type = env.entry_point.split(':')[0].split('.')[-1]
+        envs[env_type].add(env.id)
+    return envs
+
+
+def list_gym_envs() -> List[str]:
+    envs = []
+    for k, v in defaultdict_gym_envs().items():
+        for x in v:
+            envs.append(x)
+    return envs
+
+
+def np2torch_obs(obs: np.ndarray, low: np.ndarray, high: np.ndarray) -> torch.tensor:
+    obs = (obs - low) / (high - low)
+    obs = obs.astype(np.float32)
+    if len(obs.shape) == 4:
+        obs = np.transpose(obs, (0, 3, 1, 2))
+    elif len(obs.shape) == 3:
+        obs = np.transpose(obs, (2, 0, 1))
+        obs = np.expand_dims(obs, 0)
+    obs = torch.from_numpy(obs)
+
+    return obs
+
+
+def training_log_msg(update, actor_loss, critic_loss, policy_entroy, episode_reward, episode_length, args):
+    update_msg = "update#{}".format(update+1,)
+    timestep_msg = "timestep#{}".format((update+1)*args.n_steps)
+    actor_loss_msg = "actor_loss={0:.6f}".format(actor_loss)
+    critic_loss_msg = "critic_loss={0:.6f}".format(critic_loss)
+    policy_entroy_msg = "policy_entropy={0:.6f}".format(policy_entroy)
+    episode_reward_msg = "average_episode_reward={0:.2f}".format(episode_reward) if episode_reward is not None else ''
+    episode_length_msg = "average_episode_length={0:.2f}".format(episode_length) if episode_length is not None else ''
+
+    frame_width = max([len(update_msg),
+                       len(timestep_msg),
+                       len(actor_loss_msg),
+                       len(critic_loss_msg),
+                       len(policy_entroy_msg),
+                       len(episode_length_msg),
+                       len(episode_reward_msg)])
+
+    msg = '\n|' + '-' * frame_width + '|\n' +\
+          '|' + update_msg + ' ' * (frame_width-len(update_msg)) + '|\n' + \
+          '|' + timestep_msg + ' ' * (frame_width-len(timestep_msg)) + '|\n' + \
+          '|' + actor_loss_msg + ' ' * (frame_width-len(actor_loss_msg)) + '|\n' + \
+          '|' + critic_loss_msg + ' ' * (frame_width-len(critic_loss_msg)) + '|\n' + \
+          '|' + policy_entroy_msg + ' ' * (frame_width-len(policy_entroy_msg)) + '|\n' + \
+          '|' + episode_reward_msg + ' ' * (frame_width - len(episode_reward_msg)) + '|\n' + \
+          '|' + episode_length_msg + ' ' * (frame_width - len(episode_length_msg)) + '|\n' + \
+          '|' + '-' * frame_width + '|'
+
+    return msg
+
+
+def playing_log_msg(total_rewards):
+    msg_ = "Total Reward per Agent: {}".format(str([total_rewards])[2:-2])
+    msg = '\n|' + '-' * len(msg_) + '|\n' + \
+          '|' + msg_ + '|\n' + \
+          '|' + '-' * len(msg_) + '|'
+
+    return msg
+
+
+def save_checkpoint(model, optimizer, epoch, critic_loss, actor_loss, policy_entropy, average_episode_reward, average_episode_length, path, args, first):
+
+    try:
+        if first:
+            with open(os.path.join(os.path.dirname(path), 'config.json'), 'w', encoding='utf-8') as f:
+                json.dump({k: str(v) for k, v in vars(args).items()}, f, ensure_ascii=False, indent=4)
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'critic_loss': critic_loss,
+            'actor_loss' : actor_loss,
+            'policy_entropy' : policy_entropy,
+            'average_episode_reward' : average_episode_reward,
+            'average_episode_length': average_episode_length,
+        }, path)
+    except FileNotFoundError as fe:
+        os.makedirs(os.path.dirname(path))
+        if first:
+            with open(os.path.join(os.path.dirname(path), 'config.json'), 'w', encoding='utf-8') as f:
+                json.dump({k: str(v) for k,v in vars(args).items()}, f, ensure_ascii=False, indent=4)
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'critic_loss': critic_loss,
+            'actor_loss': actor_loss,
+            'policy_entropy': policy_entropy,
+            'average_episode_reward': average_episode_reward,
+            'average_episode_length': average_episode_length,
+        }, path)
